cc0bd3d2f8d1a3a4955c0d932747dd44193a0e27
Mikal Morello <mikalmorello@gmail.com>
2019-08-22 23:05:25 -0400

    JS - Add additional functions and tests

--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,36 @@
 const assert = require('chai').assert;
 const app = require('../app.js');
+//const sayHello = require('../app').sayHello;
+//const addNumbers = require('../app').addNumbers;
+
+// Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5,5);
 
 describe('App', function(){
-  it('app should return hello', function(){
-    assert.equal(app(), 'hello');
-  })
+  describe('sayHello', function(){
+    it('sayHello should return hello', function(){
+  //    assert.equal(app(), 'hello');
+  //    let result = app.sayHello();
+      assert.equal(sayHelloResult, 'hello');
+    });
+
+    it('sayHello should return type string', function(){
+  //      let result = app.sayHello();
+        assert.typeOf(sayHelloResult, 'string');
+    });
+  });
+  
+  describe('addNumber', function(){
+    it('addNumbers should be above 5', function(){
+  //    let result = app.addNumbers(5,5);
+      assert.isAbove(addNumbersResult, 5);
+    });
+
+      it('addNumbers should return type number', function(){
+  //    let result = app.addNumbers(5,5);
+      assert.typeOf(addNumbersResult, 'number');
+    });
+  });
+  
 })
